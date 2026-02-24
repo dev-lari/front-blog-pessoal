b25dc8f0584e150eb8e14ccb0b596cfef2d7aa91
Larissa <larisouzadev@gmail.com>
2026-02-23 21:19:20 -0300

    implementação das configurações de postagens e inicio do perfil

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -26,10 +26,10 @@ function Navbar() {
                     <Link to = '/home' className="text-2x1 font-bold"> Blog Pessoal </Link>
 
                     <div className='flex gap-4'>
-                        Postagens
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                        Perfil
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
                         <Link to= '' onClick={logout} className='hover:undlerline'>Sair</Link>
                     </div>
                 </div>
